bb2b39d1850e20263128972e9c0d1f61538eb99d
Renan Rhoden <renan.ostwald@zallpy.com>
2020-02-20 17:03:52 -0300

    adding animations

--- app/src/main/java/com/renanrhoden/tweetfeelings/feature/tweetslisting/TweetsListingFragment.kt
+++ app/src/main/java/com/renanrhoden/tweetfeelings/feature/tweetslisting/TweetsListingFragment.kt
@@ -3,6 +3,8 @@ package com.renanrhoden.tweetfeelings.feature.tweetslisting
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -11,12 +13,13 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.renanrhoden.tweetfeelings.R
 import com.renanrhoden.tweetfeelings.databinding.TweetsListingFragmentBinding
 import com.renanrhoden.tweetfeelings.util.EndlessRecyclerViewScrollListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class TweetsListingFragment : Fragment() {
+class TweetsListingFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
     private val viewModel: TweetsListingViewModel by viewModel()
     private val adapter: TweetsListingAdapter by lazy {
@@ -49,12 +52,22 @@ class TweetsListingFragment : Fragment() {
             }
         })
 
+        binding.swipeRefreshLayout.setOnRefreshListener(this)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
         return binding.root
     }
 
     private fun observeViewModel() {
         viewModel.tweets.observe(this, Observer {
+            binding.swipeRefreshLayout.isRefreshing = false
             adapter.updateItems(it)
+            if (it.isEmpty()) {
+                showEmpty()
+            } else {
+                hideEmpty()
+            }
         })
 
         viewModel.errorFetch.observe(this, Observer {
@@ -66,11 +79,32 @@ class TweetsListingFragment : Fragment() {
                 TweetsListingFragmentDirections.actionTweetsListingFragmentToFeelingFragment(it)
             findNavController().navigate(directions)
         })
+
+        viewModel.loading.observe(this, Observer {
+            binding.loading.visibility = if (it) {
+                hideEmpty()
+                VISIBLE
+            } else {
+                INVISIBLE
+            }
+        })
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel.fetchTweets()
+    override fun onRefresh() {
+        viewModel.refreshTweets()
     }
 
+    private fun showEmpty() {
+        binding.emptyLottie.apply {
+            visibility = VISIBLE
+            playAnimation()
+        }
+    }
+
+    private fun hideEmpty() {
+        binding.emptyLottie.apply {
+            visibility = INVISIBLE
+            cancelAnimation()
+        }
+    }
 }
--- app/src/main/java/com/renanrhoden/tweetfeelings/feature/tweetslisting/TweetsListingViewModel.kt
+++ app/src/main/java/com/renanrhoden/tweetfeelings/feature/tweetslisting/TweetsListingViewModel.kt
@@ -15,34 +15,44 @@ class TweetsListingViewModel(
     private val ln: GetSentimentUseCase
 ) : ViewModel() {
     val tweets = MutableLiveData<MutableList<Tweet>>(mutableListOf())
+    val user = MutableLiveData<String>()
     val feeling = SingleLiveEvent<Float>()
     val errorFetch = SingleLiveEvent<String>()
+    val loading = SingleLiveEvent<Boolean>()
     private val disposable = CompositeDisposable()
     private val INITIAL_MAX_ID = 0L
     private var lastTweetId = INITIAL_MAX_ID
 
     fun fetchTweets() {
-        val maxId = if (lastTweetId == INITIAL_MAX_ID) null else lastTweetId
-        getTweetsUseCase.getTweets("renanrhoden", maxId)
-            .subscribeBy(
-                onSuccess = {
-                    tweets.value?.addAll(it)
-                    tweets.value = tweets.value
-                    lastTweetId = it.last().id
-                },
-                onError = {
-                    errorFetch.value = it.message
-                }
-            ).addTo(disposable)
+        loading.value = true
+        user.value?.let {
+            val maxId = if (lastTweetId == INITIAL_MAX_ID) null else lastTweetId - 1
+            getTweetsUseCase.getTweets(it, maxId)
+                .subscribeBy(
+                    onSuccess = {
+                        loading.value = false
+                        tweets.value?.addAll(it)
+                        tweets.value = tweets.value
+                        lastTweetId = it.lastOrNull()?.id ?: INITIAL_MAX_ID
+                    },
+                    onError = {
+                        loading.value = false
+                        errorFetch.value = it.message
+                    }
+                ).addTo(disposable)
+        }
     }
 
     fun getSentiment(tweet: Tweet) {
+        loading.value = true
         ln.getSentiment(tweet.text)
             .subscribeBy(
                 onSuccess = {
+                    loading.value = false
                     feeling.value = it.score
                 },
                 onError = {
+                    loading.value = false
                     errorFetch.value = "${it.message}"
                 }
             ).addTo(disposable)
@@ -51,5 +61,21 @@ class TweetsListingViewModel(
     override fun onCleared() {
         super.onCleared()
         disposable.dispose()
+    }
+
+    fun refreshTweets() {
+        lastTweetId = INITIAL_MAX_ID
+        tweets.value?.clear()
+        fetchTweets()
+    }
+
+    fun fetchTweetsFromUser() {
+        lastTweetId = INITIAL_MAX_ID
+        tweets.value?.clear()
+        if (user.value?.isEmpty() != false) {
+            loading.value = false
+        } else {
+            fetchTweets()
+        }
     }
 }
